ca3ae01717e95b2e16a2cfac8605c4d5f0a73ede
Essam Ali <essam.3ali@gmail.com>
2017-07-11 15:49:47 +0300

    dd

--- app/hero.service.js
+++ app/hero.service.js
@@ -5,14 +5,24 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var core_1 = require("@angular/core");
 var mock_heroes_1 = require("./mock-heroes");
+require("rxjs/add/operator/map");
+var http_1 = require("@angular/http");
 var HeroService = (function () {
-    function HeroService() {
+    function HeroService(http) {
+        this.http = http;
     }
     HeroService.prototype.getHeroes = function () {
         return Promise.resolve(mock_heroes_1.HEROES);
-    }; // stub
+    };
+    // stub
+    HeroService.prototype.getObservableHeroes = function () {
+        return this.http.get('./heroes.json').map(function (heroes) { return heroes.json(); });
+    };
     HeroService.prototype.getHeroesSlowly = function () {
         var _this = this;
         return new Promise(function (resolve) {
@@ -28,6 +38,8 @@ var HeroService = (function () {
 }());
 HeroService = __decorate([
     core_1.Injectable() //emit metadata about the service. The metadata specifies that Angular may need to inject other dependencies into this service.
+    ,
+    __metadata("design:paramtypes", [http_1.Http])
 ], HeroService);
 exports.HeroService = HeroService;
 //# sourceMappingURL=hero.service.js.map
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -2,16 +2,32 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './shared/classes/hero'
 import { HEROES } from './mock-heroes';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { Http } from '@angular/http'
 
 @Injectable() //emit metadata about the service. The metadata specifies that Angular may need to inject other dependencies into this service.
 export class HeroService {
+    constructor(private http: Http)
+    {
+
+    }
     getHeroes(): Promise<Hero[]> {
 
         return Promise.resolve(HEROES);
        
         
-    } // stub
+    }
+    // stub
+    getObservableHeroes(): Observable<Hero[]>
+    {
+        return this.http.get('./heroes.json').map(heroes => <Hero[]>heroes.json());
+    }
+
+
     getHeroesSlowly(): Promise<Hero[]> {
+
+
         return new Promise(resolve => {
             // Simulate server latency with 2 second delay
             setTimeout(() => resolve(this.getHeroes()), 2000);
