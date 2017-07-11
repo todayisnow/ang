938431996b293d4d85d7ccaf11392842eae8e170
Essam Ali <essam.3ali@gmail.com>
2017-07-11 07:16:56 +0300

    rrrr

--- app/hero.service.js
+++ app/hero.service.js
@@ -5,9 +5,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 var core_1 = require("@angular/core");
 var mock_heroes_1 = require("./mock-heroes");
 var HeroService = (function () {
@@ -31,8 +28,6 @@ var HeroService = (function () {
 }());
 HeroService = __decorate([
     core_1.Injectable() //emit metadata about the service. The metadata specifies that Angular may need to inject other dependencies into this service.
-    ,
-    __metadata("design:paramtypes", [])
 ], HeroService);
 exports.HeroService = HeroService;
 //# sourceMappingURL=hero.service.js.map
